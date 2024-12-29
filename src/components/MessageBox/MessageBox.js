a670716c72b2fdce0b681b855ac8cb62028ab9fe
Ishaan Gupta <ishaangupta02@gmail.com>
2024-12-29 17:41:30 +0530

    Added messages into the message box and created toggle button to switch between two panels

--- src/components/MessageBox/MessageBox.js
+++ src/components/MessageBox/MessageBox.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './MessageBox.css';
 import { useMessage } from "../../Context/MessageContext";
 
@@ -11,6 +11,19 @@ import TuneIcon from '@mui/icons-material/Tune';
 
 function MessageBox() {
     const { messages } = useMessage();
+
+    const [focusedMessages, setFocusedMessages] = useState(messages.slice(0, messages.length / 2));
+    const [otherMessages, setOtherMessages] = useState(messages.slice(messages.length / 2, messages.length - 1));
+
+    const [selectedMessages, setSelectedMessages] = useState(focusedMessages);
+
+    const handleFocusedClick = () => {
+        setSelectedMessages(focusedMessages);
+    }
+
+    const handleOthersClick = () => {
+        setSelectedMessages(otherMessages);
+    }
 
     return (
         <div className="messageBoxContainer">
@@ -34,6 +47,26 @@ function MessageBox() {
                     </div>
                     <TuneIcon style={{ color: '#00000099', height: '20px', width: '20px', marginLeft: 'auto' }} />
                 </div>
+            </div>
+            <div className="messageBoxToggle">
+                <button className="toggleFocused" onClick={handleFocusedClick}>Focused</button>
+                <button className="toggleOther" onClick={handleOthersClick}>Other</button>
+            </div>
+            <div className="messageBoxMessagesContainer">
+                {selectedMessages.map(message => (
+                    <div className="messageBoxMessage">
+                        <img src={message.img} />
+                        <div className="messageBoxMessageRight">
+                            <div className="messageSender">
+                                <span className="messageSenderName">{message.firstName + ' ' + message.lastName}</span>
+                                <span>{message.date}</span>
+                            </div>
+                            <p className="messageBoxMessageText">
+                                {message.text.length > 50 ? message.firstName + ": " + message.text.slice(0, 50) + '...' : message.firstName + ": " + message.text}
+                            </p>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
     )
