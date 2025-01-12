88bf613554e1a6cf6b6c8de5c555463fe54d279b
Ishaan Gupta <ishaangupta02@gmail.com>
2025-01-12 16:07:35 +0530

    Added responsive designs for the notifications page

--- src/pages/Notifications/Notifications.js
+++ src/pages/Notifications/Notifications.js
@@ -1,5 +1,10 @@
+// deps
 import React from "react";
+
+// styles
 import './Notifications.css';
+
+// components
 import LeftPane from "../../containers/Notifications/LeftPane/LeftPane";
 import MainFeed from "../../containers/Notifications/MainFeed/MainFeed";
 import RightPane from "../../containers/Notifications/RightPane/RightPane";
