0da80cec335140c014776d070d9868ae5ec5c52c
Ishaan Gupta <ishaangupta02@gmail.com>
2025-01-29 15:45:40 +0530

    Added api for fetching recommended profiles on profile page

--- src/components/Profile/MoreProfiles/MoreProfiles.js
+++ src/components/Profile/MoreProfiles/MoreProfiles.js
@@ -1,5 +1,5 @@
 // deps
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // styles
 import './MoreProfiles.css';
@@ -11,44 +11,53 @@ import CircleIcon from '@mui/icons-material/Circle';
 // constants
 import { MORE_PROFILES } from "../../../constants/texts/Profile/moreProfiles";
 
+// apis
+import { fetchRecommendedProfiles } from "../../../utils/apis/profile";
+
 function MoreProfiles() {
-    const profiles = [
-        {
-            img: 'https://media.licdn.com/dms/image/v2/D4D03AQGYxNTXYJlddQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1699309825094?e=1740614400&v=beta&t=mZFI7qaZgbNl4g8ilVpeQX5YXM3bTesolnDTaZlLdRA',
-            name: 'Sophia Johnson',
-            degree: '1st',
-            bio: 'Marketing Manager | Digital Marketing Specialist | MBA Graduate',
-            connected: true
-        },
-        {
-            img: 'https://media.licdn.com/dms/image/v2/D4D03AQGYxNTXYJlddQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1699309825094?e=1740614400&v=beta&t=mZFI7qaZgbNl4g8ilVpeQX5YXM3bTesolnDTaZlLdRA',
-            name: 'James Carter',
-            degree: '2nd',
-            bio: 'Full Stack Developer | React, Node.js, and MongoDB Expert',
-            connected: false
-        },
-        {
-            img: 'https://media.licdn.com/dms/image/v2/D4D03AQGYxNTXYJlddQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1699309825094?e=1740614400&v=beta&t=mZFI7qaZgbNl4g8ilVpeQX5YXM3bTesolnDTaZlLdRA',
-            name: 'Emily Brown',
-            degree: '3rd',
-            bio: 'UI/UX Designer | Advocate for Accessibility in Design',
-            connected: false
-        },
-        {
-            img: 'https://media.licdn.com/dms/image/v2/D4D03AQGYxNTXYJlddQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1699309825094?e=1740614400&v=beta&t=mZFI7qaZgbNl4g8ilVpeQX5YXM3bTesolnDTaZlLdRA',
-            name: 'Michael Lee',
-            degree: '1st',
-            bio: 'Data Scientist | Python & Machine Learning Specialist',
-            connected: true
-        },
-        {
-            img: 'https://media.licdn.com/dms/image/v2/D4D03AQGYxNTXYJlddQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1699309825094?e=1740614400&v=beta&t=mZFI7qaZgbNl4g8ilVpeQX5YXM3bTesolnDTaZlLdRA',
-            name: 'Rachel Taylor',
-            degree: '2nd',
-            bio: 'HR Manager | Expert in Talent Acquisition and Employee Engagement',
-            connected: true
-        }
-    ];
+    const [profiles, setProfiles] = useState([]);
+
+    useEffect(() => {
+        fetchRecommendedProfiles().then(data => setProfiles(data));
+    }, [])
+
+    // const profiles = [
+    //     {
+    //         img: 'https://media.licdn.com/dms/image/v2/D4D03AQGYxNTXYJlddQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1699309825094?e=1740614400&v=beta&t=mZFI7qaZgbNl4g8ilVpeQX5YXM3bTesolnDTaZlLdRA',
+    //         name: 'Sophia Johnson',
+    //         degree: '1st',
+    //         bio: 'Marketing Manager | Digital Marketing Specialist | MBA Graduate',
+    //         connected: true
+    //     },
+    //     {
+    //         img: 'https://media.licdn.com/dms/image/v2/D4D03AQGYxNTXYJlddQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1699309825094?e=1740614400&v=beta&t=mZFI7qaZgbNl4g8ilVpeQX5YXM3bTesolnDTaZlLdRA',
+    //         name: 'James Carter',
+    //         degree: '2nd',
+    //         bio: 'Full Stack Developer | React, Node.js, and MongoDB Expert',
+    //         connected: false
+    //     },
+    //     {
+    //         img: 'https://media.licdn.com/dms/image/v2/D4D03AQGYxNTXYJlddQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1699309825094?e=1740614400&v=beta&t=mZFI7qaZgbNl4g8ilVpeQX5YXM3bTesolnDTaZlLdRA',
+    //         name: 'Emily Brown',
+    //         degree: '3rd',
+    //         bio: 'UI/UX Designer | Advocate for Accessibility in Design',
+    //         connected: false
+    //     },
+    //     {
+    //         img: 'https://media.licdn.com/dms/image/v2/D4D03AQGYxNTXYJlddQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1699309825094?e=1740614400&v=beta&t=mZFI7qaZgbNl4g8ilVpeQX5YXM3bTesolnDTaZlLdRA',
+    //         name: 'Michael Lee',
+    //         degree: '1st',
+    //         bio: 'Data Scientist | Python & Machine Learning Specialist',
+    //         connected: true
+    //     },
+    //     {
+    //         img: 'https://media.licdn.com/dms/image/v2/D4D03AQGYxNTXYJlddQ/profile-displayphoto-shrink_200_200/profile-displayphoto-shrink_200_200/0/1699309825094?e=1740614400&v=beta&t=mZFI7qaZgbNl4g8ilVpeQX5YXM3bTesolnDTaZlLdRA',
+    //         name: 'Rachel Taylor',
+    //         degree: '2nd',
+    //         bio: 'HR Manager | Expert in Talent Acquisition and Employee Engagement',
+    //         connected: false
+    //     }
+    // ];
 
     return (
         <div className="moreProfilesContainer">
@@ -60,7 +69,7 @@ function MoreProfiles() {
                     <div className="recProfileContainer">
                         <div className="recProfile">
                             <div className="recProfileImg">
-                                <img src={profile.img} />
+                                <img src={profile.image} />
                             </div>
                             <div className="recProfileRight">
                                 <div className="recProfileTitle">
---
+++ src/utils/apis/profile.js
@@ -0,0 +1,11 @@
+const fetchRecommendedProfiles = async () => {
+    try {
+        const response = await fetch('http://localhost:9999/fetch-profile');
+        const data = response.json();
+        return data;
+    } catch (err) {
+        console.error('Error in fetching recommended profiles.');
+    }
+}
+
+export { fetchRecommendedProfiles };
